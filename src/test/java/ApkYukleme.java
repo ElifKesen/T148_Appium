362ccc5a27836ff5bd4769eaeff824f31c95c904
Elif Kesen <elifkesen@gmail.com>
2025-03-11 12:46:17 +0100

    11.03

---
+++ src/test/java/ApkYukleme.java
@@ -0,0 +1,28 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ApkYukleme {
+    AndroidDriver<AndroidElement> driver;
+
+    @Test
+    public void apkYukleme() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 148");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(MobileCapabilityType.APP, "C:\\Users\\elifk\\IdeaProjects\\T148_Appium\\Apps\\To-Do List - Schedule Planner_1.02.61.0928_APKPure.apk");
+
+        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+}
